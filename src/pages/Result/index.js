7ba3ec227661bcaa0219a7aa90f0ecab1dd4e95f
dougllasfm <douglasmunaro@gmail.com>
2022-09-26 16:46:13 -0300

    mudanças

--- src/pages/Result/index.js
+++ src/pages/Result/index.js
@@ -6,7 +6,7 @@ export function Result() {
   const location = useLocation();
 
   return (
-    <div style={{ background: "#4d0099", height: "100vh", display: "flex", flexDirection: "column",justifyContent: "center", alignItems: "center"}}>
+    <div style={{ background: "red", height: "100vh", display: "flex", flexDirection: "column",justifyContent: "center", alignItems: "center"}}>
       <h1 style={{ width: "620px", margin: "0.4rem", padding: "0.5rem 1rem", color: "#000", background: "yellow", borderRadius: "10px", fontWeight: "bold" }}>Página com styles misturando components do styled-components e dentro da própria tag html no jsx.</h1>
       <List>
         {location.state?.repos?.map((element, i) => {
@@ -17,7 +17,7 @@ export function Result() {
         to="/Search"
         style={{ padding: "0.5rem 1rem", color: "#000", background: "yellow", borderRadius: "10px", fontWeight: "bold" }}
       >
-        Voltar
+        Voltar para a página de pesquisa
       </Link>
     </div>
   );
