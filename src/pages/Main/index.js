96d613afb0fea28e312fda0a86ebbe1275c0c44f
dougllasfm <douglasmunaro@gmail.com>
2022-09-26 16:59:45 -0300

    yellow

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export function Main() {
 
   return (
-    <div style={{ background: "#4d0099", height: "100vh", display: "flex", flexDirection: "column",justifyContent: "center", alignItems: "center"}}>
+    <div style={{ background: "yellow", height: "100vh", display: "flex", flexDirection: "column",justifyContent: "center", alignItems: "center"}}>
       <h1 style={{ color: "#fff" }}>Página com styles apenas no jsx</h1>
       <Link to="/Search" style={{ background: "yellow", padding: "0.5rem 1rem", border: "0", outline: "none", marginTop: "2rem", fontSize: "1.2rem", fontWeight: "bold", cursor: "pointer"}}>Começar</Link>
     </div>
